9c9085eea031ec6c0bfb0f4a688f9a579e068b23
Shreyas Jenner <shreyasjenner4@gmail.com>
2025-02-18 22:29:06 +0530

    added state code

--- infinsm/src/App.jsx
+++ infinsm/src/App.jsx
@@ -1,44 +1,95 @@
-import React, { useCallback } from 'react';
-import {
-  ReactFlow,
-  MiniMap,
-  Controls,
-  Background,
-  useNodesState,
-  useEdgesState,
+import { 
+  ReactFlow, 
+  Background, 
+  Controls, 
+  applyEdgeChanges, 
+  applyNodeChanges,
   addEdge,
-} from '@xyflow/react';
- 
+} from "@xyflow/react";
+import { useState, useCallback } from "react";
 import '@xyflow/react/dist/style.css';
- 
+
+import { TaskNode, endNode, startNode,forkNode, joinNode } from "./components/State";
+
+// get node types
+const nodeTypes = {taskNode: TaskNode, startNode: startNode, endNode: endNode, 
+  forkNode: forkNode, joinNode: joinNode, 
+};
+
 const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  {
+    id: '1',
+    data: { label: 'Hello' },
+    position: { x: 0, y: 0 },
+    type: 'taskNode',
+  },
+  {
+    id: '2',
+    data: { label: 'World', body: 'this is body' },
+    position: { x: 100, y: 100 },
+    type: 'taskNode'
+  },
+  {
+    id: '3',
+    position: {x: 150, y: 150},
+    type: 'endNode'
+  },
+  {
+    id: '4',
+    position: {x: 50, y: 50},
+    type: 'startNode',
+  },
+  {
+    id: '5',
+    position: {x: 10, y: 10},
+    type: 'forkNode',
+  },
+  {
+    id: '6',
+    position: {x: 30, y: 10},
+    type: 'joinNode',
+  },
 ];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
  
-export default function App() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
- 
+const initialEdges = [];
+
+function App() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [],
+  );
+  
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [],
+  );
+
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
-    [setEdges],
+    [],
   );
- 
+  
+
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
+    <div style={{height: '100vh', width: '100vw'}}>
+      <ReactFlow 
+      colorMode="dark"
+      nodes={nodes} 
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      nodeTypes={nodeTypes}
+      fitView
       >
+        <Background />
         <Controls />
-        <MiniMap />
-        <Background variant="dots" gap={12} size={1} />
       </ReactFlow>
     </div>
-  );
-}
+  )
+};
+
+export default App;
---
+++ infinsm/src/components/State.jsx
@@ -0,0 +1,83 @@
+/*
+JSX file that implements the states of the FSM
+The different states are:
+1. start : represents the start of the machine
+2. end : represents the final state of the machine
+3. task : represents a task in the machine
+4. fork : used when concurrent tasks need to split
+5. join : used when concurrent tasks need to join back
+*/
+
+import { Handle, Position } from "@xyflow/react";
+
+const startNodeStyle = {
+    width: 20,
+    height: 20,
+    borderRadius: '50%',
+}
+
+const startNodeTextStyle = {
+    fontSize: 9,
+    top: '50%',
+    left: '50%',
+    transform: 'translate(0%, 20%)',
+}
+
+const forkNodeStyle = {
+    width: 5,
+    height: 100,
+};
+
+
+function startNode() {
+    return (
+        <div className="react-flow__node-default" style={startNodeStyle}>
+            <div className="react-flow__node-header" style={startNodeTextStyle}>Start</div>
+            <Handle type="source" position={Position.Right} />
+        </div>
+        );
+}
+
+function endNode() {
+    return (
+        <div className="react-flow__node-default" style={startNodeStyle}>
+            <Handle type="target" position={Position.Left} />
+            <div className="react-flow__node-header" style={startNodeTextStyle}>Stop</div>
+        </div>
+    );
+}
+
+function TaskNode({data}) {
+    return (
+        <div className="react-flow__node-default">
+            <Handle type="target" position={Position.Left} />
+            <div className="react-flow__node-header">{data.label}</div>
+            <div className="react-flow__node-body">{data.body}</div>
+            <Handle type="source" position={Position.Right} />
+        </div>
+    );
+}
+
+function forkNode() {
+    return (
+        <div className="react-flow__node-default" style={forkNodeStyle}>
+            <Handle type="source" position={Position.Right} />
+        </div>
+    );
+}
+
+function joinNode() {
+    return (
+        <div className="react-flow__node-default" style={forkNodeStyle}>
+            <Handle type="target" position={Position.Left} />
+        </div>
+    );
+}
+
+export {
+    startNode,
+    endNode,
+    TaskNode,
+    forkNode,
+    joinNode
+}
